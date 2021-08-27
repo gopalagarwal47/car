d1ded415ac6aa390c55c95f6fed7115ee8c1e5f1
agarwalgopal47 <agarwalgopal47@gmail.com>
2021-08-27 19:47:39 +0530

    car

--- hello/utils.py
+++ hello/utils.py
@@ -12,17 +12,17 @@ def message_mail(name, phone, address, textarea):
 	
 	s.login(MAIL_ID, PASS) 
 	
-	body  = name + " wrote to you :" + phone + address + textarea  + "\n. His email is: " 
+	body  = name + " wrote to you :" + phone  + "\n" + address + "\n" + textarea  + "\n. His email is: " 
 	# message to be sent 
 
 	message = MIMEMultipart()
 
 	message['Subject'] = "Somebody send you a new message!"
 	message["From"] = MAIL_ID
-	message["To"] = "agarwalgopal47@gmail.com"
+	message["To"] = " Ravibharat2720@gmail.com"
 	message.attach(MIMEText(body, 'plain'))
 
-	s.sendmail(MAIL_ID, "agarwalgopal47@gmail.com", message.as_string())
+	s.sendmail(MAIL_ID, " Ravibharat2720@gmail.com", message.as_string())
 
 	# terminating the session 
 	s.quit() 
@@ -45,3 +45,28 @@ def message_mail(name, phone, address, textarea):
 
 #      # terminating the session
 #      s.quit()
+
+
+
+def message_mail(name,phone,address,company,model):
+	s = smtplib.SMTP('smtp.gmail.com', 587)  
+	s.starttls() 
+	
+	s.login(MAIL_ID, PASS) 
+	
+	body  = name + " wrote to you :" + phone + " \n" + address + "\n "  + company + "\n " + model + " " + "\n. His email is: " 
+	# message to be sent 
+
+	message = MIMEMultipart()
+
+	message['Subject'] = "Somebody send you a new message!"
+	message["From"] = MAIL_ID
+	message["To"] = " Ravibharat2720@gmail.com"
+	message.attach(MIMEText(body, 'plain'))
+
+	s.sendmail(MAIL_ID, " Ravibharat2720@gmail.com", message.as_string())
+
+	# terminating the session 
+	s.quit() 
+
+	
