c1d687fc5cbdc6ef9b17c8e70ac86ef3efd0bdcf
agarwalgopal47 <agarwalgopal47@gmail.com>
2021-08-27 19:56:28 +0530

    car

--- hello/settings.py
+++ hello/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-tc6w+@wkutkej@&i$nzi5*2lwe9-ggt&6m!o=3*@a*cci!*&vt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["balajicarclinic.herokuapp.com"]
 
 
 
